e959c18d10b5c7ad7169463aeae8957d574cbf93
Suhyun Park <me@shiftpsh.com>
2024-11-26 00:45:29 +0900

    fix: Hanoi site score

--- app/2025/data/hanoi/region.ts
+++ app/2025/data/hanoi/region.ts
@@ -14,9 +14,39 @@ const VIETNAM_PRELIM_TEAMS = prelimsTeamsWithUnofficial.filter((x) => {
   return true;
 });
 
+const VIETNAM_LAST_YEAR_FOREIGN_INSTITUTIONS = [
+  // #1
+  "Seoul National University",
+  // #2, 3, 6, 7, 12
+  "National University of Singapore",
+  // #4, 11
+  "National Taiwan University",
+  // #10
+  "Universitas Indonesia",
+  // #16, 20, 26
+  "Nanyang Technological University",
+  // #29
+  "University of the Thai Chamber of Commerce",
+  // #32, 42, 57, 82
+  "Chulalongkorn University",
+  // #39
+  "Bina Nusantara University",
+  // #75
+  "Ateneo de Manila University",
+  // #103
+  "Chiang Mai University",
+];
+
 const VIETNAM_REGION_SCORE: RegionScoreArgs = {
-  univs: 63,
-  teams: 125,
+  // 11/26 18:30 (UTC+7):
+  // Domestic univs will be at least 58.
+  // # foreign universities is unknown, so it is calculated from
+  // the last year's number of foreign teams.
+  univs: 58 + VIETNAM_LAST_YEAR_FOREIGN_INSTITUTIONS.length,
+  // It will be likely that the number of teams will be 140.
+  // (120 Vietnamese teams + 20 foreign teams)
+  // https://www.olp.vn/tin-t%E1%BB%A9c/olympic-icpc/th%C3%B4ng-b%C3%A1o
+  teams: 140,
   foreignTeams: 20,
   teamsPrelim: countTeams(VIETNAM_PRELIM_TEAMS),
   univsPrelim: countUniversities(VIETNAM_PRELIM_TEAMS),
--- app/2025/hanoi/page.tsx
+++ app/2025/hanoi/page.tsx
@@ -11,7 +11,11 @@ const Page = () => {
     <Container>
       <RegionHeader region={Hanoi} />
       <Card>
-        <b>Hanoi:</b> Regional team stats are from the last year (Hue City).
+        <b>Hanoi:</b> 140 teams (120 Vietnamese teams + 20 foreign teams) from
+        58 domestic universities and unknown count of foreign universities are
+        expected:
+        https://www.olp.vn/tin-t%E1%BB%A9c/olympic-icpc/th%C3%B4ng-b%C3%A1o; #
+        foreign universities is calculated from the last year&lsquo;s number.
       </Card>
       <RegionTeams region={Hanoi} />
     </Container>
