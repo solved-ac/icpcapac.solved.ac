b00583d5720e6edcddee4c8048b96c54cb6b477e
Suhyun Park <me@shiftpsh.com>
2025-11-22 18:03:42 +0900

    feat: Seoul concluded

--- app/2026/data/seoul/region.tsx
+++ app/2026/data/seoul/region.tsx
@@ -1,7 +1,7 @@
+import { INSTITUTION_REGION_MAP } from "@/app/data/institutions/institution";
 import { Seoul as Seoul2025 } from "../../../2025/data/seoul/region";
 // import { INSTITUTION_REGION_MAP } from "../institution";
 import {
-  countTeamNames,
   countTeams,
   countUniversities,
   regionScore,
@@ -10,23 +10,16 @@ import {
 import { Regional, RegionalStatus } from "../types";
 
 import prelimsStandings from "./Korea2026NationalFirstStandings.json";
-import foreignTeams from "./Seoul2026ForeignTeams.json";
-import domesticTeams from "./Seoul2026DomesticTeams.json";
-// import regionalStandings from "./Seoul2025Standings.json";
-
-// Expected # of domestic teams is 78.
-// see https://icpckorea.org/archives/3356
-// `domesticTeams` is generated from the list from the above page.
+import regionalStandings from "./Seoul2026Standings.json";
 
 const KOREA_REGION_SCORE: RegionScoreArgs = {
-  univs: countUniversities(foreignTeams, domesticTeams),
-  teams: countTeamNames(foreignTeams, domesticTeams),
-  // foreignTeams: countTeams(
-  //   regionalStandings.filter(
-  //     (team) => INSTITUTION_REGION_MAP.get(team.institution) !== "KOR"
-  //   )
-  // ),
-  foreignTeams: countTeamNames(foreignTeams),
+  univs: countUniversities(regionalStandings),
+  teams: countTeams(regionalStandings),
+  foreignTeams: countTeams(
+    regionalStandings.filter(
+      (team) => INSTITUTION_REGION_MAP.get(team.institution) !== "KOR"
+    )
+  ),
   teamsPrelim: countTeams(prelimsStandings),
   univsPrelim: countUniversities(prelimsStandings),
 };
@@ -36,18 +29,15 @@ export const Seoul: Regional = {
   site: "Seoul",
   region: "KOR",
   url: "https://icpckorea.org/2025-seoul/regional",
-  status: RegionalStatus.preliminariesFinished,
+  status: RegionalStatus.regionalsFinished,
   score: regionScore(KOREA_REGION_SCORE),
   scoreDetails: KOREA_REGION_SCORE,
-  regionalTeams: [...foreignTeams, ...domesticTeams],
+  scoreboard: regionalStandings,
   lastYear: Seoul2025,
   disclaimer: (
     <>
-      Regional domestic team list is converted from:{" "}
-      <a href="https://icpckorea.org/archives/3356">
-        https://icpckorea.org/archives/3356
-      </a>
-      .
+      Scoreboard is not yet published in the ICPC system, but is official since
+      I am in the technical committee.
     </>
   ),
 };
