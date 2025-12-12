eb6b8704a0e595b0891d0dfc72b352a94852b69b
Suhyun Park <me@shiftpsh.com>
2025-12-13 02:29:17 +0900

    feat: HCMC Concluded

--- app/2026/data/hcmc/region.tsx
+++ app/2026/data/hcmc/region.tsx
@@ -1,30 +1,32 @@
-import { Regional, RegionalStatus } from "../types";
-import { isVNMHighSchool } from "@/app/data/institutions/institution";
+import {
+  INSTITUTION_REGION_MAP,
+  isVNMHighSchool,
+} from "@/app/data/institutions/institution";
 import {
   countTeams,
   countUniversities,
   regionScore,
   RegionScoreArgs,
 } from "../regionScore";
+import { Regional, RegionalStatus } from "../types";
 
 import { Hanoi as Hanoi2025 } from "@/app/2025/data/hanoi/region";
 
-import regionalStandings from "./Vietnam2026NationalScoreboard.json";
-import foreignTeams from "./HCMC2026ForeignTeams.json";
+import regionalStandings from "./HCMC2026Standings.json";
+import prelimsStandings from "./Vietnam2026NationalScoreboard.json";
 
-const VIETNAM_PRELIM_TEAMS = regionalStandings.filter(
+const VIETNAM_PRELIM_TEAMS = prelimsStandings.filter(
   (team) => !isVNMHighSchool(team.institution)
 );
 
 const VIETNAM_REGION_SCORE: RegionScoreArgs = {
-  univs: 61, //countUniversities(regionalStandings),
-  teams: 129, //countTeams(regionalStandings),
-  // foreignTeams: countTeams(
-  //   regionalStandings.filter(
-  //     (team) => INSTITUTION_REGION_MAP.get(team.institution) !== "VNM"
-  //   )
-  // ),
-  foreignTeams: foreignTeams.length,
+  univs: countUniversities(regionalStandings),
+  teams: countTeams(regionalStandings),
+  foreignTeams: countTeams(
+    regionalStandings.filter(
+      (team) => INSTITUTION_REGION_MAP.get(team.institution) !== "VNM"
+    )
+  ),
   teamsPrelim: countTeams(VIETNAM_PRELIM_TEAMS),
   univsPrelim: countUniversities(VIETNAM_PRELIM_TEAMS),
 };
@@ -34,9 +36,18 @@ export const HCMC: Regional = {
   site: "HCMC",
   region: "VNM",
   url: "https://acm-icpc.olp.vn/",
-  status: RegionalStatus.preparing,
+  status: RegionalStatus.regionalsFinished,
   score: regionScore(VIETNAM_REGION_SCORE),
   scoreDetails: VIETNAM_REGION_SCORE,
+  scoreboard: regionalStandings,
   lastYear: Hanoi2025,
-  disclaimer: <>It is confirmed that 130 teams from 62 univs will attend.</>,
+  disclaimer: (
+    <>
+      Scoreboard is not yet published in the ICPC system. It is converted from:{" "}
+      <a href="https://icpc.vnoi.info/contest/icpc_regional_2025/ranking/?tags=">
+        https://icpc.vnoi.info/contest/icpc_regional_2025/ranking/?tags=
+      </a>{" "}
+      at 25-12-13.
+    </>
+  ),
 };
