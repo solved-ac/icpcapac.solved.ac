0fba4f41d03c56f36528807e09f55b46f7638a19
shiftpsh <shiftpsh@users.noreply.github.com>
2025-12-12 05:27:02 +0000

    deploy: d2a8bde059696bc3ad92816dfa84580cba130d32

--- _next/static/chunks/app/2026/jakarta/page-2d99c20d96291255.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{3636:(e,s,r)=>{Promise.resolve().then(r.bind(r,614))},614:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>l});var _=r(9358),a=r(874);let l=(0,_.A)(a.p)}},e=>{var s=s=>e(e.s=s);e.O(0,[545,928,334,860,580,120,441,517,358],()=>s(3636)),_N_E=e.O()}]);
